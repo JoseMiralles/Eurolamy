fe6bce91019a223630b430e74d30647343274bba
Jose <magicomiralles@gmail.com>
2019-06-05 20:31:40 -0400

    Tryin to ge the animation to work.

---
+++ src/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    var controller = new ScrollMagic.Controller();
+
+
+    const triggerElement ='#table_animation_trigger';
+    var itemName = "";
+    for (i = 1; i <= 11; i++) {
+        itemName = "#OD" + i;
+    }
+
+    console.log("READY FUNC ENDED");
+});
