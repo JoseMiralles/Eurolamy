e62664808ef71f413ac36a22ec6efc2f6775f9b7
Jose <magicomiralles@gmail.com>
2019-06-06 19:35:32 -0400

    Improved animation

--- src/scripts.js
+++ src/scripts.js
@@ -3,22 +3,29 @@ $(document).ready(function () {
 
     var tweenAnimIn = new TimelineMax();
     var tweenAnimIn2 = new TimelineMax();
+    var tweenAnimIn3 = new TimelineMax();
     const triggerElement ='#table_animation_trigger';
     var itemName = "";
     for (i = 1; i <= 11; i++) {
         itemName = "#OD" + i;
         tweenAnimIn.from(itemName, 0.3, {
-            y:-200,
+            x:-100,
             autoAlpha: 0,
             ease:Expo.easeOut
         });
         itemName = "#OD" + (i + 1);
-        tweenAnimIn2.from(itemName, 0.5, {
-            y:-200,
+        tweenAnimIn2.from(itemName, 0.4, {
+            x:-100,
             autoAlpha: 0,
             ease:Expo.easeOut
         });
-        i++
+        itemName = "#OD" + (i + 2);
+        tweenAnimIn3.from(itemName, 0.5, {
+            x:-100,
+            autoAlpha: 0,
+            ease:Expo.easeOut
+        });
+        i+=2;
     }
 
     console.log("READY FUNC ENDED");
