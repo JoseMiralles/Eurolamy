0fd9d0f20a34e8284951d55afe976657151428a9
Jose <magicomiralles@gmail.com>
2019-06-06 18:43:36 -0400

    Animated table

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,24 @@
 $(document).ready(function () {
     var controller = new ScrollMagic.Controller();
 
-
+    var tweenAnimIn = new TimelineMax();
+    var tweenAnimIn2 = new TimelineMax();
     const triggerElement ='#table_animation_trigger';
     var itemName = "";
     for (i = 1; i <= 11; i++) {
         itemName = "#OD" + i;
+        tweenAnimIn.from(itemName, 0.3, {
+            y:-200,
+            autoAlpha: 0,
+            ease:Expo.easeOut
+        });
+        itemName = "#OD" + (i + 1);
+        tweenAnimIn2.from(itemName, 0.5, {
+            y:-200,
+            autoAlpha: 0,
+            ease:Expo.easeOut
+        });
+        i++
     }
 
     console.log("READY FUNC ENDED");
