9e34da0ccf4139442579fca76d83f0036c9f89b2
Jose <magicomiralles@gmail.com>
2019-06-06 22:05:25 -0400

    Finishied the scroll animation

--- src/scripts.js
+++ src/scripts.js
@@ -1,32 +1,53 @@
 $(document).ready(function () {
     var controller = new ScrollMagic.Controller();
 
-    var tweenAnimIn = new TimelineMax();
-    var tweenAnimIn2 = new TimelineMax();
-    var tweenAnimIn3 = new TimelineMax();
-    const triggerElement ='#table_animation_trigger';
+    ShowDeskAnimation();
+    AddDeskScrollAnimation();
+
+    console.log("READY FUNC ENDED");
+});
+
+///Shows the office desk animation immediately.
+var ShowDeskAnimation = function(){
+    const tweenAnimIn = new TimelineMax();
+    const tweenAnimIn2 = new TimelineMax();
+    const tweenAnimIn3 = new TimelineMax();
     var itemName = "";
+    const xpos = -100, ypos = -70;
     for (i = 1; i <= 11; i++) {
         itemName = "#OD" + i;
-        tweenAnimIn.from(itemName, 0.3, {
-            x:-100,
+        tweenAnimIn.from(itemName, 0.1, {
+            x:xpos,
+            y:ypos,
             autoAlpha: 0,
             ease:Expo.easeOut
         });
         itemName = "#OD" + (i + 1);
-        tweenAnimIn2.from(itemName, 0.4, {
-            x:-100,
+        tweenAnimIn2.from(itemName, 0.2, {
+            x:xpos,
+            y:ypos,
             autoAlpha: 0,
             ease:Expo.easeOut
         });
         itemName = "#OD" + (i + 2);
-        tweenAnimIn3.from(itemName, 0.5, {
-            x:-100,
+        tweenAnimIn3.from(itemName, 0.3, {
+            x:xpos,
+            y:ypos,
             autoAlpha: 0,
             ease:Expo.easeOut
         });
         i+=2;
     }
+};
 
-    console.log("READY FUNC ENDED");
-});
+///Adds the scroll animation for the desk svg.
+var AddDeskScrollAnimation = function(){
+    const triggerElement ='#table_animation_trigger';
+    const controller = new ScrollMagic.Controller();
+
+    for(i = 1; i <= 11; i++){
+        new ScrollMagic.Scene({triggerElement: triggerElement,duration:300})
+        .setTween("#OD"+i, {y:(0-(i*50)), autoAlpha:0})
+        .addTo(controller);
+    }
+};
