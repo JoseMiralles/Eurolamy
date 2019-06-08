20c92f5d4328a5b92db002de977b0b9ecc2cf4e0
Jose <magicomiralles@gmail.com>
2019-06-08 14:58:12 -0400

    Improved contact section

--- src/scripts.js
+++ src/scripts.js
@@ -4,13 +4,18 @@ $(document).ready(function () {
     avoid timeline issues.*/
     ShowDeskAnimation();
 
+    /*TODO: loop this*/
     AddParalax("#port_target_1", "#port_trigger_1", "#prot_shroud_1");
     AddParalax("#port_target_2", "#port_trigger_2");
     AddParalax("#port_target_3", "#port_trigger_3");
     AddParalax("#port_target_4", "#port_trigger_4");
     AddParalax("#port_target_5", "#port_trigger_5");
 
+    CreatFadeInScrollAnimation("#Whatsapp_card", "#Contact_card_trigger", 200, 400);
+    CreatFadeInScrollAnimation("#facebook_card", "#Contact_card_trigger", 200, 200);
+
     console.log("READY FUNC ENDED");
+
 });
 
 var AddParalax = function (targetElementID, triggerElementID, shroudElementID) {
@@ -25,8 +30,8 @@ var AddParalax = function (targetElementID, triggerElementID, shroudElementID) {
             }, 2);
     }
     parallaxTl.from(targetElementID, 4, {
-        y: '-100%',
-        scale: 1.2,
+        y: '-20%',
+        scale: 2,
         ease: Power0.easeNone
     }, 0);
 
@@ -47,19 +52,48 @@ var ScrollToElement = function (targetElementID) {
     controller.scrollTo(targetElementID);
 }
 
-/*Button animation*/
-var IntroduceButton = function () {
+/*Bounces the given item to the specified x position and back*/
+var BounceItemX = function (targetID, xpos) {
     const tweenAnimIn = new TimelineMax();
     tweenAnimIn
-        .to("#contact_button", 1, {
-            x: 50,
+        .to(targetID, 1, {
+            x: xpos,
             ease: Expo.easeOut
         })
-        .to("#contact_button", 1, {
+        .to(targetID, 1, {
             x: 0,
             ease: Bounce.easeOut
         });
+        return tweenAnimIn;
 };
+/*Bounces the given item to the specified y position and back*/
+var BounceItemY = function(targetID, ypos){
+    const tweenAnimIn = new TimelineMax();
+    tweenAnimIn
+    .to(targetID, 1,{
+        y: ypos,
+        ease: Expo.easeOut
+    })
+    .to(targetID, 1, {
+        y: 0,
+        ease: Bounce.easeOut
+    });
+    return tweenAnimIn;
+}
+/*Fades in item from the given y position*/
+var CreatFadeInScrollAnimation = function(targetElementID, triggerElementID, ypos, duration){
+    var parallaxTl = new TimelineMax();
+    parallaxTl.from(targetElementID, 3,{
+        y: ypos,
+        autoAlpha: 0
+    },);
+
+    const controller = new ScrollMagic.Controller();
+    new ScrollMagic.Scene({
+        triggerElement: triggerElementID,
+        duration: duration})
+        .setTween(parallaxTl).addTo(controller);
+}
 
 /*HERO ANIMATIONS*/
 ///Shows the office desk animation immediately.
@@ -79,7 +113,8 @@ var ShowDeskAnimation = function () {
         }, "-=1.7").eventCallback("onComplete", function () {
             /*Add the scroll animation untill the first animation is done*/
             AddDeskScrollAnimation();
-            IntroduceButton();
+            BounceItemX("#contact_button", 50).eventCallback("onComplete", 
+            BounceItemY("#previous_projects_text", 50));
         });
     }
 };
