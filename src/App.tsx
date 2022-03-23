a4b22a5c09cd413cc2eda3a1fc01e30c83e4a1ff
Jose Miralles <josemiralles1991@gmail.com>
2022-03-23 10:43:19 -0400

    Fixed overflow issue on the hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
+import FacebookSection from './components/FacebookSection';
 import HeroSection from './components/HeroSection';
 import ProjectItem from './components/ProjectItem';
 import { finishedProjects } from './data';
 
 function App() {
 
-  const projectItems = finishedProjects.map((project) => {
-    return (
-      <ProjectItem
-        name={project.name}
-        description={project.description}
-        images={project.images}
-      />
-    );
-  });
-
   return (
     <div className="App">
       <HeroSection/>
-      {/* {projectItems} */}
+      <FacebookSection/>
     </div>
   );
 }
---
+++ src/components/FacebookSection.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const FacebookSection = (): JSX.Element => {
+
+    return (
+        <div id="facebook-section">
+            <div className="content">
+
+                <div className="left">
+                    Left
+                </div>
+
+                <div className="right">
+                    Right
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default FacebookSection;
