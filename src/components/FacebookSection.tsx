8e25ddc9d0dc7028681bd9f22e7bdbe7fe4963e3
Jose Miralles <josemiralles1991@gmail.com>
2022-03-23 15:43:02 -0400

    Added new 3d models to blender file, and started integrating Facebook.

--- src/components/FacebookSection.tsx
+++ src/components/FacebookSection.tsx
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const FacebookSection = (): JSX.Element => {
+
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        setLoaded(true);
+    }, []);
 
     return (
         <div id="facebook-section">
             <div className="content">
 
-                <div className="left">
-                    Left
+                <div className="left grow">
+                    <div className="title">Facebook</div>
+                    <div className="description">Vea nuestros ultimos projectos.</div>
                 </div>
 
-                <div className="right">
-                    Right
+                <div className="right grow">
+                    {loaded && <div
+                        className="fb-page"
+                        data-href="https://www.facebook.com/EUROLAMY/"
+                        data-tabs="timeline"
+                        data-width="500"
+                        data-height=""
+                        data-small-header="false"
+                        data-adapt-container-width="true"
+                        data-hide-cover="false"
+                        data-show-facepile="true">
+                        <blockquote
+                            cite="https://www.facebook.com/EUROLAMY/"
+                            className="fb-xfbml-parse-ignore">
+                            <a href="https://www.facebook.com/EUROLAMY/">
+                                Eurolamy Muebles de oficina
+                            </a>
+                        </blockquote>
+                    </div>}
                 </div>
 
             </div>
